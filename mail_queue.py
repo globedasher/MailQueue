8f18664577d27f18be44a52aaca1b836bcf519eb
globedasher <globe.dasher@gmail.com>
2018-08-08 23:12:25 -0700

    CLI section functional.

--- mail_queue.py
+++ mail_queue.py
@@ -14,10 +14,10 @@ class Node():
     """
     next_node = None
     ident = 0
-    value = 0
+    date = 0
 
     def __init__(self, data, ident):
-        self.value = data
+        self.date = data
         self.ident = ident
 
 
@@ -26,6 +26,22 @@ class MailQueue():
     MailQueue class.
     """
     head = None
+
+    def _find_node(self, ident):
+        if not self.head:
+            return None
+        currentNode = self.head
+        if currentNode.ident == ident:
+            self.head = currentNode.next_node
+            currentNode.next_node = None
+            return currentNode
+        while currentNode:
+            if not currentNode.next_node:
+                return None
+            if currentNode.ident == ident:
+                return currentNode
+            currentNode = currentNode.next_node
+
 
     def _pop_node(self, ident):
         #print("_pop_node")
@@ -49,17 +65,17 @@ class MailQueue():
         # If no node with that ident is found, return None
         return None
 
-    def _insert_node(self, value, ident):
+    def _insert_node(self, date, ident):
         # Create and insert a node
-        new_node = Node(value, ident)
+        new_node = Node(date, ident)
         # if no head exists on the queue
         if not self.head:
             self.head = new_node
-        # if the new node value is less than the current head
-        elif new_node.value < self.head.value:
+        # if the new node date is less than the current head
+        elif new_node.date < self.head.date:
             new_node.next_node = self.head
             self.head = new_node
-        # else find the location to insert it
+        # else pop the location to insert it
         else:
             currentNode = self.head
             while currentNode:
@@ -68,31 +84,12 @@ class MailQueue():
                 if not currentNode.next_node:
                     currentNode.next_node = new_node
                     break
-                elif (new_node.value > currentNode.value
-                    and new_node.value < currentNode.next_node.value):
+                elif (new_node.date > currentNode.date
+                    and new_node.date < currentNode.next_node.date):
                     new_node.next_node = currentNode.next_node
                     currentNode.next_node = new_node
                     break
                 currentNode = currentNode.next_node
-
-    def create(self, value, ident):
-        """
-        Create a value into the queue.  If a node with the specified ident is
-        in the queue, it will be removed and a new node will be created based
-        on the new value. If is not in the queue, it will be added at correct
-        location.
-        """
-        #print("Create", value,"Ident", ident)
-        found_on_next_node = self._pop_node(ident)
-        # if found_on_next_node:
-        #     print("Found node:"
-        #          ,found_on_next_node.value
-        #          ,found_on_next_node.ident
-        #          )
-        self._insert_node(value, ident)
-
-    def update(self, value, ident):
-        self.create(value, ident)
 
     def read_nodes(self):
         """
@@ -106,11 +103,11 @@ class MailQueue():
         currentNode = self.head
         node_number = 0
         while currentNode:
-            #print(str(currentNode.value), str(currentNode.ident), str(node_number))
+            #print(str(currentNode.date), str(currentNode.ident), str(node_number))
 
             # Create a nested dictionary for each node in the queue
             return_node = {
-                    "date": currentNode.value,
+                    "date": currentNode.date,
                     "ident": currentNode.ident,
                     }
 
@@ -122,6 +119,50 @@ class MailQueue():
             #print(node_number)
         return str(return_dict)
 
+    def create(self, message):
+        """
+        Create a value into the queue.  If a node with the specified ident is
+        in the queue, it will be removed and a new node will be created based
+        on the new value. If is not in the queue, it will be added at correct
+        location.
+        """
+        command, ident = message.split(":")
+        #print(command, ident)
+        date = datetime.datetime.now()
+        offset = datetime.timedelta(days = 5)
+        date = date + offset
+
+        # Remove the node from the 
+        found_on_next_node = self._pop_node(ident)
+        self._insert_node(date, ident)
+
+    def read(self, message):
+        print("Read method")
+        print(message)
+        command, ident = message.split(":")
+        print("read", ident)
+        node = self._find_node(ident)
+        print(node.date, node.ident)
+        return node
+
+    def update(self, message):
+        print("Update")
+        print(message)
+        command, ident = message.split(":")
+        date = datetime.datetime.now()
+        offset = datetime.timedelta(days = 5)
+        date = date + offset
+
+        found_on_next_node = self._pop_node(ident)
+        self._insert_node(date, ident)
+
+    def delete(self, message):
+        print(message)
+        command, ident = message.split(":")
+        print("Delete", ident)
+        found_on_next_node = self._pop_node(ident)
+
+
     def end_node(self):
         """cnsert
         I don't know why I built this...
@@ -130,7 +171,7 @@ class MailQueue():
         currentNode = self.head
         while currentNode:
             if not currentNode.next_node:
-                return currentNode.value
+                return currentNode.date
             currentNode = currentNode.next_node
 
     def expire(self):
@@ -138,8 +179,8 @@ class MailQueue():
         This method expires nodes.
         """
         #print("Check for expired node")
-        if self.head and self.head.value <= datetime.datetime.now():
-            print("Expire Date:", self.head.value
+        if self.head and self.head.date <= datetime.datetime.now():
+            print("Expire Date:", self.head.date
                  ,"Ident:", self.head.ident
                  )
             self.head = self.head.next_node
@@ -193,6 +234,74 @@ class ControlComms():
         print("Creating Controls")
 
 
+class MailClient():
+
+    def __init__(self):
+        print("Init MailClient")
+
+    def get_ipc_socket(self):
+        context = zmq.Context()
+        print("Creating socket to process.")
+        socket = context.socket(zmq.REQ)
+        rc = socket.connect("ipc:///tmp/mail_queue_ipc")
+        #print(rc)
+        return socket
+
+    def send_command(self, command, socket):
+        socket.send_string(command)
+        message = socket.recv()
+        message = message.decode("utf-8")
+        message = eval(message)
+        printer = pprint.PrettyPrinter(indent=4)
+        printer.pprint(message)
+
+    def read_queue(self):
+        """
+        Get the queue
+        """
+        socket = self.get_ipc_socket()
+        command = "read_queue"
+        socket.send_string(command)
+
+        message = socket.recv()
+        message = message.decode("utf-8")
+        message = eval(message)
+        #print(type(message))
+        #print("Received reply: %s" % (message))
+        printer = pprint.PrettyPrinter(indent=4)
+        printer.pprint(message)
+
+    def create(self, param):
+        """
+        Send an create command to the process.
+        """
+        print("Create id", param)
+
+        socket = self.get_ipc_socket()
+
+        print("Sending create for ident %s" % param)
+        command = "create: %s" % (param)
+        socket.send_string(command)
+
+        message = socket.recv()
+        print("Received reply: %s" % (message))
+
+    def read(self, param):
+        socket = self.get_ipc_socket()
+        command = "read: %s" % (param)
+        self.send_command(command, socket)
+
+    def update(self, param):
+        socket = self.get_ipc_socket()
+        command = "update: %s" % (param)
+        self.send_command(command, socket)
+
+    def delete(self, param):
+        socket = self.get_ipc_socket()
+        command = "delete: %s" % (param)
+        self.send_command(command, socket)
+
+
 def test_inserts(mail_queue, tests):
     value = 0
     #print(value)
@@ -210,29 +319,6 @@ def test_inserts(mail_queue, tests):
         mail_queue.create(data, ident)
     return mail_queue
 
-def comms_loop(send_pipe):
-    print("comms_loop")
-
-    listen_context = zmq.Context()
-    listen_socket = listen_context.socket(zmq.REP)
-    listen_socket.bind("ipc:///tmp/mail_queue_ipc")
-
-
-    while True:
-
-        #message = listen_socket.recv()
-        #print("Received request: %s" % message)
-        listen_socket.send(b"World")
-
-        #print("Here")
-        message = message.decode("utf-8")
-        #print(message)
-        if message == "create":
-            #print("Over here")
-            send_pipe.send("create")
-        elif message == "read_queue":
-            send_pipe.send("read_queue")
-        #sys.exit()
 
 def main_loop(mail_queue, streams):
     print("main_loop")
@@ -254,16 +340,7 @@ def main_loop(mail_queue, streams):
             message = listen_socket.recv()
             print("Received message: %s" % message)
             message = message.decode("utf-8")
-            command, ident = message.split(":")
-            #print(command, ident)
-            if command == "create":
-                #print("create")
-                data = datetime.datetime.now()
-                offset = datetime.timedelta(days = 5)
-                data = data + offset
-                mail_queue.create(data, ident)
-                listen_socket.send_string("0")
-            elif command == "read_queue":
+            if "read_queue" in message:
                 listen_socket.send_string(str(mail_queue.read_nodes()))
                 #return_dict = mail_queue.read_nodes()
                 #print("return_list", return_list)
@@ -271,6 +348,24 @@ def main_loop(mail_queue, streams):
                     #listen_socket.send_string("No nodes")
                 #else:
                     #listen_socket.send_string(return_list)
+            elif "create" in message:
+                #print("create")
+                mail_queue.create(message)
+                listen_socket.send_string("0")
+            elif "read" in message:
+                response = mail_queue.read(message)
+                return_dict = {
+                        "date": response.date,
+                        "ident": response.ident,
+                        }
+                print(return_dict)
+                listen_socket.send_string(str(return_dict))
+            elif "update" in message:
+                mail_queue.update(message)
+                listen_socket.send_string("0")
+            elif "delete" in message:
+                mail_queue.delete(message)
+                listen_socket.send_string("0")
             else:
                 pass
 
@@ -301,54 +396,9 @@ def process_init(tests):
     # context where a single parameter is in a tupple with a free element
     # after.
 
-    #comms_loop_process = Process(target=comms_loop, args=(send_pipe, ))
-    #comms_loop_process.start()
-
     mail_queue_process = Process(target=main_loop
                        , args=(mail_queue, streams))
     mail_queue_process.start()
-
-def create(param=None):
-    """
-    Send an create command to the process.
-    """
-    print("Create id", param)
-
-    context = zmq.Context()
-
-    print("Transmitting commands to process.")
-    socket = context.socket(zmq.REQ)
-    rc = socket.connect("ipc:///tmp/mail_queue_ipc")
-    #print(rc)
-
-    print("Sending create for ident %s" % param)
-    command = "create: %s" % (param)
-    socket.send_string(command)
-
-    message = socket.recv()
-    print("Received reply: %s" % (message))
-
-def read_queue():
-    """
-    Get the queue
-    """
-    context = zmq.Context()
-
-    print("Transmitting commands to process.")
-    socket = context.socket(zmq.REQ)
-    rc = socket.connect("ipc:///tmp/mail_queue_ipc")
-    #print(rc)
-
-    command = "read_queue:"
-    socket.send_string(command)
-
-    message = socket.recv()
-    message = message.decode("utf-8")
-    message = eval(message)
-    #print(type(message))
-    #print("Received reply: %s" % (message))
-    printer = pprint.PrettyPrinter(indent=4)
-    printer.pprint(message)
 
 def get_args():
     """
@@ -372,15 +422,22 @@ def get_args():
                         , help=help_text
                         )
 
-    help_text = """See all nodes in queue."""
+    help_text = """Read a node in the queue."""
     parser.add_argument(  "-r"
-                        , "--read-queue"
+                        , "--read"
+                        , dest="read_node"
+                        , default=None
+                        , help=help_text
+                        )
+
+    help_text = """See all nodes in queue."""
+    parser.add_argument(  "--read-queue"
                         , dest="read_queue"
                         , action="store_true"
                         , help=help_text
                         )
 
-    help_text = """Create a value with an ident."""
+    help_text = """Update a value with an ident."""
     parser.add_argument("-u"
                         , "--update"
                         , dest="update"
@@ -388,7 +445,7 @@ def get_args():
                         , help=help_text
                         )
 
-    help_text = """Create a value with an ident."""
+    help_text = """Delete a value with an ident."""
     parser.add_argument("-d"
                         , "--delete"
                         , dest="delete"
@@ -414,14 +471,20 @@ def main():
     if args.process:
         process_init(args.tests)
     elif args.create:
-        create(args.create)
+        client = MailClient()
+        client.create(args.create)
     elif args.read_queue:
-        read_queue()
+        client = MailClient()
+        client.read_queue()
+    elif args.read_node:
+        client = MailClient()
+        client.read(args.read_node)
     elif args.update:
-        # Faking the update for now...
-        create(args.update)
+        client = MailClient()
+        client.update(args.update)
     elif args.delete:
-        print("No delete function yet.")
+        client = MailClient()
+        client.delete(args.delete)
 
     else:
         # This else will allow dev to run this script with no args to just run
